84827d72c01d2e868e03c20045bf74be260c6677
Tyler Levine <tyler@bitgo.com>
2019-10-24 14:13:23 -0700

    SERV-263: Deprecate statics repository

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './base';
-export * from './coins';
-export * from './networks';
-export * from './errors';
-export { OfcCoin } from './ofc';
-export { UtxoCoin } from './utxo';
-export { AccountCoin, Erc20Coin, StellarCoin } from './account';
-export { CoinMap } from './map';
